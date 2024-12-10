9b09dc88d18631351d36ae2ccdc134139b08dbec
mathilde2801 <mathilde72m@gmail.com>
2024-12-10 15:29:05 +0100

    Ajout du contrôleur de connexion et de la vue de connexion pour le projet Game Collection

---
+++ Views/Connection.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="connection">
+        <div class="titre">
+            <h1>Se connecter a Game Collection</h1>
+        </div>
+        <div class="form">
+            <form action="index.php" method="post">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" required>
+                <label for="password">Mot de passe</label>
+                <input type="password" name="password" id="password" required>
+                <input type="submit" value="Se connecter">
+                <a href="index.php?action=inscription">Pas encore inscrit ?</a>
+            </form>
+    </div>
+</body>
+</html>
