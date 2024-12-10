19cd89ab6e1c20cdd0c671e7ef32aec02b946df9
mathilde2801 <mathilde72m@gmail.com>
2024-12-10 15:53:29 +0100

    Implement signup functionality with validation and user insertion logic

--- Controllers/SingUpController.php
+++ Controllers/SingUpController.php
@@ -1,3 +1,30 @@
 <?php
-
+require_once('Models/SingUpModel.php');
+isset($_SERVER('REQUEST_METHOD')=='POST'){
+    if(isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['confirmpassword'])){
+        $nom = htmlspecialchars($_POST['nom']);
+        $prenom = htmlspecialchars($_POST['prenom']);
+        $email = htmlspecialchars($_POST['email']);
+        $password = htmlspecialchars($_POST['password']);
+        $confirmpassword = htmlspecialchars($_POST['confirmpassword']);
+        if($password == $confirmpassword){
+            $user = selectemail($email);
+            if(!$user){
+                $password = password_hash($password, PASSWORD_DEFAULT);
+                $insert = insertuser($nom, $prenom, $email, $password);
+                if($insert){
+                    header('Location: index.php');
+                else{
+                    $error = "Erreur lors de l'inscription";
+                }
+            }else{
+                $error = "Email déjà utilisé";
+            }
+        }else{
+            $error = "Les mots de passe ne correspondent pas";
+        }
+    }
+}
+}
+require_once('Views/SingUp.php');
 ?>
