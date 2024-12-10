055922637802d23b66b1bbd516653085d3c3dfaf
Blazes35 <loridam2@gmail.com>
2024-12-10 16:28:46 +0100

    addWithoutForm

--- index.php
+++ index.php
@@ -4,10 +4,13 @@ session_set_cookie_params(86400 * 30, "/");
 session_start();
 
 // routage
-$page = $_POST['page'] ?? isset($_GET['page']) && $_GET['page'] != '' ? $_GET['page'] : 'Home';
+$page = $_POST['page'] ?? isset($_GET['page']) && $_GET['page'] != '' ? $_GET['page'] : 'WithoutForm';
 switch ($page) {
     case 'Home':
         include '';
+        break;
+    case 'WithoutForm':
+        include 'Controllers/AddGameWithoutForm.php';
         break;
     default:
         include './Views/Error404.php';
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit25ba9adda1bfcfce2adce065d787ed53
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit25ba9adda1bfcfce2adce065d787ed53', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit25ba9adda1bfcfce2adce065d787ed53', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInit25ba9adda1bfcfce2adce065d787ed53
         call_user_func(\Composer\Autoload\ComposerStaticInit25ba9adda1bfcfce2adce065d787ed53::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit25ba9adda1bfcfce2adce065d787ed53::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
