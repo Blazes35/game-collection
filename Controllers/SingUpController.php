5c1e4d68a968209789c03c7323611353eb850dbc
mathilde2801 <mathilde72m@gmail.com>
2024-12-16 10:19:33 +0100

    Fix signup form input fields and improve error handling in signup process

--- Controllers/SingUpController.php
+++ Controllers/SingUpController.php
@@ -1,16 +1,14 @@
 <?php
 require_once('../Models/SingUpModel.php');
-$model = new SingUpModel();
-echo "ok";
+$model = new SingUpModel(); 
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['confirmpassword'])) {
-        echo "ok";
         $nom = htmlspecialchars($_POST['nom']);
         $prenom = htmlspecialchars($_POST['prenom']);
         $email = htmlspecialchars($_POST['email']);
         $password = htmlspecialchars($_POST['password']);
         $confirmpassword = htmlspecialchars($_POST['confirmpassword']);
-        var_dump($nom,$prenom,$email,$password,$confirmpassword);
         if ($password == $confirmpassword) {
             $user = $model->selectemail($email);
             if (!$user) {
@@ -19,15 +17,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 if ($insert) {
                     header('Location: index.php');
                     exit();
-                } else {
-                    $error = "Erreur lors de l'inscription";
-                }
-            } else {
-                $error = "Email déjà utilisé";
+                } 
             }
-        } else {
-            $error = "Les mots de passe ne correspondent pas";
-        }
+        } 
     }
 }
 require_once('../Views/SingUp.php');
