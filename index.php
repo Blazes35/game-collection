ec953b61844793690f3ca8f4aa2b983cab75a976
Blazes35 <loridam2@gmail.com>
2024-12-16 11:31:05 +0100

    DB load

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ session_name('game-collection');
 session_set_cookie_params(86400 * 30, "/");
 session_start();
 
+
 // routage
 /*$page = $_POST['page'] ?? isset($_GET['page']) && $_GET['page'] != '' ? $_GET['page'] : 'Home';
 switch ($page) {
