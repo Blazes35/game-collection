fe7c6151ba92097f7f1de439cbf0fa0e57a816d1
Blazes35 <loridam2@gmail.com>
2024-12-16 11:32:21 +0100

    DB load

--- index.php
+++ index.php
@@ -3,6 +3,22 @@ session_name('game-collection');
 session_set_cookie_params(86400 * 30, "/");
 session_start();
 
+// Load dependencies
+require 'vendor/autoload.php';
+
+// Load environment variables
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// Load database
+require 'db.php';
+
+try {
+    $pdo = init_database();
+    echo 'Bravo ! Vous avez réussi a déployer votre application !';
+} catch (PDOException $e) {
+    echo 'Attention ! Erreur de connexion à la base de données : ' . $e->getMessage();
+}
 
 // routage
 /*$page = $_POST['page'] ?? isset($_GET['page']) && $_GET['page'] != '' ? $_GET['page'] : 'Home';
