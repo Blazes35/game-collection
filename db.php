0a81d82efbde4d8d41c237d7ed24f4807be6eebf
Blazes35 <loridam2@gmail.com>
2024-12-16 11:38:44 +0100

    exception

--- db.php
+++ db.php
@@ -8,8 +8,12 @@ function init_database()
     $user = $_ENV['DB_USER'];
     $password = $_ENV['DB_PASSWORD'];
 
+    try{
     $dsn = "mysql:host=$host;port=$port;dbname=$dbname";
     $pdo = new PDO($dsn, $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $pdo;
+    } catch (PDOException $e) {
+        throw new PDOException('Erreur de connexion à la base de données : ' . $e->getMessage());
+    }
 }
