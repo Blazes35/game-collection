88d1448db4df3dee2c8f6b516aa87dd5bdec36b7
mathilde2801 <mathilde72m@gmail.com>
2025-01-07 15:15:04 +0100

    Remove echo output for email selection in SignUpController

--- Controllers/SignUpController.php
+++ Controllers/SignUpController.php
@@ -4,7 +4,6 @@ $model = new SignUpModel();
 
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    echo $model->select();
     if (isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['confirmpassword'])) {
         
         $nom = htmlspecialchars($_POST['nom']);
