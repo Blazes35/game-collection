041e8b35bf303fd60bdbed18ef1ca399f621500e
AyScorp <aymericcherbonnier@gmail.com>
2025-01-07 14:19:54 +0100

    Correction des jointures dans ClassementModel pour assurer une récupération correcte des données

--- Models/ClassementModel.php
+++ Models/ClassementModel.php
@@ -11,9 +11,9 @@ class ClassementModel {
                 ROUND(utilisateur_jeu.temps_de_jeu / 60, 2) AS total_heures
             FROM utilisateur
             JOIN utilisateur_jeu 
-                ON utilisateur.id = utilisateur_jeu.id_utilisateur
+                ON utilisateur.id = utilisateur_jeu.id
             JOIN jeu 
-                ON utilisateur_jeu.id_jeu = jeu.id
+                ON utilisateur_jeu.id = jeu.id
             ORDER BY utilisateur_jeu.temps_de_jeu DESC;
         ");
         $stmt->execute();
