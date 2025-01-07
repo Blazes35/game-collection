496bc4a345390991b10f17a910cd01a3fb778b16
mathilde2801 <mathilde72m@gmail.com>
2025-01-07 15:25:56 +0100

    Update redirect location in ConnectionController after successful login

--- Controllers/ConnectionController.php
+++ Controllers/ConnectionController.php
@@ -11,7 +11,7 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
             if(password_verify($password, $user['password'])){
                 $_SESSION['id'] = $user['id'];
                 $_SESSION['email'] = $user['email'];
-                header('Location: Controllers/HomeController.php');
+                header('Location: ?page=Home');
             }else{
                 echo "<div class= 'errormdp'>Mot de passe incorrect</div>";
             }
