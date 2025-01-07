710d37a4ebc82a426d5321d7f935aafe3f053f54
mathilde2801 <mathilde72m@gmail.com>
2025-01-07 22:14:27 +0100

    Add ConnectionModel for email selection functionality in ProfilModel

--- Models/ProfilModel.php
+++ Models/ProfilModel.php
@@ -1,4 +1,16 @@
 <?php
 require_once('Models/DBModel.php');
+class ConnectionModel extends DBModel {
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function selectemail($email) {
+        $stmt = self::$db->prepare("SELECT * FROM utilisateur WHERE email = :email");
+        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+}
 
 ?>
