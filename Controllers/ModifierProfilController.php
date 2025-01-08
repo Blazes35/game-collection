bc9b899a400e5cf19d2d8068892abd3042afa26f
mathilde2801 <mathilde72m@gmail.com>
2025-01-08 13:51:48 +0100

    Add user session check and retrieval in ModifierProfilController

--- Controllers/ModifierProfilController.php
+++ Controllers/ModifierProfilController.php
@@ -1,5 +1,10 @@
 <?php
 require_once('Models/ModifierProfilModel.php');
 $model = new ModifierProfilModel();
+
+if(isset($_SESSION['id'])){
+    $user = $model->selectuser($_SESSION['id']);
+}
+
 require_once('Views/ModifierProfil.php');
 ?>
