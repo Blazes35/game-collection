288561a2301fd68ebaa03dfbe849bccd769f3f9e
mathilde2801 <mathilde72m@gmail.com>
2025-01-08 14:05:35 +0100

    Fix password confirmation check in ModifierProfilController and hash password only if they match

--- Controllers/ModifierProfilController.php
+++ Controllers/ModifierProfilController.php
@@ -11,11 +11,12 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
         $nom = htmlspecialchars($_POST['nom']);
         $prenom = htmlspecialchars($_POST['prenom']);
         $email = htmlspecialchars($_POST['email']);
-        $password = password_hash(htmlspecialchars($_POST['password']), PASSWORD_DEFAULT);
-        $confirmpassword = password_hash(htmlspecialchars($_POST['confirmpassword']), PASSWORD_DEFAULT);
-        if($password != $confirmpassword){
+        
+        
+        if($_POST['password'] != $_POST['confirmpassword']){
             echo "<div class= 'errorpassword'>Les mots de passe ne correspondent pas</div>";
         }else{
+            $password = password_hash(htmlspecialchars($_POST['password']), PASSWORD_DEFAULT);
             $model->updateuser($_SESSION['id'], $nom, $prenom, $email, $password);
             $_SESSION['email'] = $email;
             $_SESSION['prenom'] = $prenom;
