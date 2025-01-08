3c06ef6a48742b96258e79ef0e4ac52cddb0bcc1
mathilde2801 <mathilde72m@gmail.com>
2025-01-08 14:00:37 +0100

    Add user profile update functionality in ModifierProfilController and ModifierProfilModel

--- Controllers/ModifierProfilController.php
+++ Controllers/ModifierProfilController.php
@@ -6,5 +6,25 @@ if(isset($_SESSION['id'])){
     $user = $model->selectuser($_SESSION['id']);
 }
 
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    if(isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['email']) && isset($_POST['password'])&& isset($_POST['confirmpassword'])){
+        $nom = htmlspecialchars($_POST['nom']);
+        $prenom = htmlspecialchars($_POST['prenom']);
+        $email = htmlspecialchars($_POST['email']);
+        $password = password_hash(htmlspecialchars($_POST['password']), PASSWORD_DEFAULT);
+        $confirmpassword = password_hash(htmlspecialchars($_POST['confirmpassword']), PASSWORD_DEFAULT);
+        if($password != $confirmpassword){
+            echo "<div class= 'errorpassword'>Les mots de passe ne correspondent pas</div>";
+        }else{
+            $model->updateuser($_SESSION['id'], $nom, $prenom, $email, $password);
+            $_SESSION['email'] = $email;
+            $_SESSION['prenom'] = $prenom;
+            header('Location: ?page=Profil');
+        }
+        
+        
+    }
+}
+
 require_once('Views/ModifierProfil.php');
 ?>
--- Models/ModifierProfilModel.php
+++ Models/ModifierProfilModel.php
@@ -11,5 +11,15 @@ class ModifierProfilModel extends DBModel{
         $stmt->execute();
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
+
+    public function updateuser($id, $nom, $prenom, $email, $password) {
+        $stmt = self::$db->prepare("UPDATE utilisateur SET nom = :nom, prenom = :prenom, email = :email, password = :password WHERE id = :id");
+        $stmt->bindParam(':id', $id, PDO::PARAM_STR);
+        $stmt->bindParam(':nom', $nom, PDO::PARAM_STR);
+        $stmt->bindParam(':prenom', $prenom, PDO::PARAM_STR);
+        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+        $stmt->bindParam(':password', $password, PDO::PARAM_STR);
+        return $stmt->execute();
+    }
 }
 ?>
