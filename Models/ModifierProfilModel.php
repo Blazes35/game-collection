0fe39ae4a26796cc1a5ac7d4228440faa3e51edd
mathilde2801 <mathilde72m@gmail.com>
2025-01-08 13:49:10 +0100

    Add user selection method to ModifierProfilModel for data retrieval

--- Models/ModifierProfilModel.php
+++ Models/ModifierProfilModel.php
@@ -1,3 +1,15 @@
 <?php
+require_once('Models/DBModel.php');
+class ModifierProdilModel extends DBModel{
+    public function __construct() {
+        parent::__construct();
+    }
 
+    public function selectuser($id) {
+        $stmt = self::$db->prepare("SELECT * FROM utilisateur WHERE id = :id");
+        $stmt->bindParam(':id', $id, PDO::PARAM_STR);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+}
 ?>
