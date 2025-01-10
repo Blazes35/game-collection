2fcabb9bfefa13efac4fc159fe57d4bb19b48655
Guillaume Dodin <gdodin53@gmail.com>
2025-01-10 10:53:12 +0100

    Update form action to empty string in Form view
    Replaced the form's action attribute with an empty string to ensure the form submission targets the current page. This adjustment simplifies form handling and avoids hardcoded dependency on the FormController path.

--- Views/Form.php
+++ Views/Form.php
@@ -14,7 +14,7 @@ ob_start();
     <div class="container">
         <h1>Ajouter un jeu à sa bibliothèque</h1>
         <p>Le jeu que vous souhaitez ajouter n'existe pas ! Vous pouvez le créer, celui-ci sera automatiquement ajouté à votre bibliothèque !</p>
-        <form method="post" action="Controllers/FormController.php">
+        <form method="post" action="">
             <div class="form-group">
                 <label for="nom">Nom du jeu</label>
                 <input type="text" id="nom" name="nom" placeholder="Nom du jeu">
