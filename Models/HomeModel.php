5d0409ab6aaa39164fbfa5a154fc32f6b128b3a0
AyScorp <aymericcherbonnier@gmail.com>
2025-01-10 10:44:01 +0100

    Correction de la requête SQL dans HomeModel pour utiliser l'identifiant du jeu au lieu de l'identifiant du joueur

--- Models/HomeModel.php
+++ Models/HomeModel.php
@@ -13,7 +13,7 @@ class HomeModel extends DBModel {
             SELECT jeu.nom, jeu.image_url, jeu.plateforme, utilisateur_jeu.temps_de_jeu 
             FROM jeu 
             JOIN utilisateur_jeu ON jeu.id = utilisateur_jeu.id 
-            WHERE utilisateur_jeu.id = :playerId");
+            WHERE utilisateur_jeu.id = jeu.id");
         $stmt->bindParam(':playerId', $playerId, PDO::PARAM_INT);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
