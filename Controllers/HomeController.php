8b840bae6912b900644d9d3b50236c83a04d5c45
Blazes35 <loridam2@gmail.com>
2025-01-10 10:55:43 +0100

    change routage

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -1,23 +1,22 @@
 <?php
 require_once 'Models/HomeModel.php';
-
+/*
 $model = new HomeModel();
-    $playerData = $model->getPlayerData($playerId);
-    $playerGames = $model->getPlayerGames($playerId);
+$playerData = $model->getPlayerData($playerId);
+$playerGames = $model->getPlayerGames($playerId);
 
-    $playerId = $_SESSION['id'];
+$playerId = $_SESSION['id'];
 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        // Traitement des données POST
-        $playerId = isset($_POST['player_id']) ? (int)$_POST['player_id'] : null;
-
-        if ($playerId) {
-            displayHome($playerId);
-        } else {
-            echo "ID du joueur non fourni.";
-        }
+    // Traitement des données POST
+    $playerId = isset($_POST['player_id']) ? (int)$_POST['player_id'] : null;
+    if ($playerId) {
+        displayHome($playerId);
+    } else {
+        echo "ID du joueur non fourni.";
     }
+}*/
 
-require_once 'Views/Home.php';
+require 'Views/Home.php';
 ?>
