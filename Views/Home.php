485efbb009c17ccdd735471c3a73fe0d8c41eedf
AyScorp <aymericcherbonnier@gmail.com>
2025-01-11 15:37:00 +0100

    Refactor la liste des jeux en utilisant des divs pour une meilleure présentation

--- Views/Home.php
+++ Views/Home.php
@@ -9,19 +9,19 @@ if (isset($playerData) && isset($playerGames)) {
 
     echo "<h2>Bonjour, $playerName !</h2>";
     echo "<h3>Voici vos jeux :</h3>";
-    echo "<ul>";
+    echo "<div class='game-list'>";
     foreach ($games as $game) {
         $gameName = $game['nom'];
         $gameImage = $game['image_url'];
         $gamePlatform = $game['plateforme'];
         $gameHours = round($game['temps_de_jeu'] / 60, 2); // Convertir les minutes en heures
 
-        echo "<li>";
+        echo "<div class='card' style='background-image: url($gameImage);'>";
         echo "<img src='$gameImage' alt='$gameName' style='width:100px;height:100px;'>";
         echo "<strong>$gameName</strong> sur $gamePlatform - $gameHours heures de jeu";
-        echo "</li>";
+        echo "</div>";
     }
-    echo "</ul>";
+    echo "</div>";
 } else {
     echo "<p>Les données du joueur ne sont pas disponibles.</p>";
 }
