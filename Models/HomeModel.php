c4a78f0f8692df562d339b25c18ad70a0fa99a52
Blazes35 <loridam2@gmail.com>
2025-01-11 14:20:44 +0100

    change HomeController.php

--- Models/HomeModel.php
+++ Models/HomeModel.php
@@ -7,18 +7,16 @@ class HomeModel extends DBModel {
         $stmt->execute([$playerId]);
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
-/*
+
     public function getPlayerGames($playerId) {
         $stmt = $this->getDB()->prepare("
             SELECT jeu.nom, jeu.image_url, jeu.plateforme, utilisateur_jeu.temps_de_jeu 
             FROM jeu 
             JOIN utilisateur_jeu ON jeu.id = utilisateur_jeu.jeu_id
-            WHERE utilisateur_jeu.utilisateur_id = utilisateur_id
-        ");
+            WHERE utilisateur_jeu.utilisateur_id = utilisateur_id");
         $stmt->execute([$playerId]);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    */
 }
 ?>
