422c959aa1087a5ce579bbec319e1ecfa231ed4b
Blazes35 <loridam2@gmail.com>
2025-01-11 15:41:59 +0100

    change FormModel.php

--- Models/FormModel.php
+++ Models/FormModel.php
@@ -45,7 +45,7 @@ class FormModel extends DBModel {
         }else {
             $query = "SELECT * FROM jeu WHERE nom LIKE :game";
             $stmt = self::$db->prepare($query);
-            $game = "'%".$game."%'";
+            $game = "%".$game."%";
             $stmt->bindParam(':game', $game);
             $stmt->execute();
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
