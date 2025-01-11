e3ebd9bc93a3c69790a445e675913c30edfdbd83
Blazes35 <loridam2@gmail.com>
2025-01-11 15:12:13 +0100

    change FormModel.php

--- Models/FormModel.php
+++ Models/FormModel.php
@@ -20,8 +20,17 @@ class FormModel extends DBModel {
     }
 
     public function insertUserGame($userId, $gameName) {
+        $query = "SELECT * FROM utilisateur_jeu WHERE utilisateur_id = :utilisateur_id AND jeu_id = :jeu_id";
+        $stmt = self::$db->prepare($query);
+        $stmt->bindParam(':utilisateur_id', $userId);
+        $stmt->bindParam(':jeu_id', $gameName);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($result) {
+            return;
+        }
         $query = "INSERT INTO utilisateur_jeu (utilisateur_id, jeu_id, temps_de_jeu) VALUES (:utilisateur_id, :jeu_id, 0)";
-        $stmt = $this->getDB()->prepare($query);
+        $stmt = self::$db->prepare($query);
         $stmt->bindParam(':utilisateur_id', $userId);
         $stmt->bindParam(':jeu_id', $gameName);
         $stmt->execute();
@@ -30,12 +39,13 @@ class FormModel extends DBModel {
     public function getGames($game){
         if ($game == ""){
             $query = "SELECT * FROM jeu";
-            $stmt = $this->getDB()->prepare($query);
+            $stmt = self::$db->prepare($query);
             $stmt->execute();
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
         }else {
-            $query = "SELECT * FROM jeu WHERE nom LIKE %:game%";
-            $stmt = $this->getDB()->prepare($query);
+            $query = "SELECT * FROM jeu WHERE nom LIKE :game";
+            $stmt = self::$db->prepare($query);
+            $game = "%" . $game . "%";
             $stmt->bindParam(':game', $game);
             $stmt->execute();
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
