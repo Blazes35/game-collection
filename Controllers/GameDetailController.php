d043f8d9c48af2f0f30b5f2bbadf58e812d7e222
Blazes35 <loridam2@gmail.com>
2025-01-12 16:53:52 +0100

    addGameDetail.php

--- Controllers/GameDetailController.php
+++ Controllers/GameDetailController.php
@@ -3,10 +3,9 @@ require_once 'Models/GameDetailModel.php';
 $model = new GameDetailModel();
 
 $gameId = $_SESSION['gameId'];
-var_dump($gameId);
 $game = $model->getGame($gameId);
 var_dump($game);
-$gameName = $game[0]['nom'];
+$gameName = $game['nom'];
 
 require 'Views/GameDetailView.php';
 ?>
