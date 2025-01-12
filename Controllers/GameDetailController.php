6552d82cb3c161ad8e5d79173313bbce1ed8e8b7
Blazes35 <loridam2@gmail.com>
2025-01-12 17:01:10 +0100

    addGameDetail.php

--- Controllers/GameDetailController.php
+++ Controllers/GameDetailController.php
@@ -11,7 +11,7 @@ $gameImage = $game['image_url'];
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
     if(isset($_POST['temps_de_jeu'])){
         $model->changeTime($_SESSION['id'], $gameId, $_POST['temps_de_jeu']);
-        header('Location: /Profil');
+        header('Location: /');
         exit();
     }
 }
