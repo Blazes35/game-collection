8b6405f249d1df4309353cf5916e4fbfa877885b
mathilde2801 <mathilde72m@gmail.com>
2025-01-12 17:50:44 +0100

    Add player game time calculations and update ranking display

--- Controllers/ClassementController.php
+++ Controllers/ClassementController.php
@@ -2,7 +2,28 @@
 require_once 'Models/ClassementModel.php';
 $model = new ClassementModel();
 $compte=0;
-
+function cumulerTempsDeJeu($joueurId) {
+    $tempsTotal = 0;
+    $parties = $model->getPartiesByJoueur($joueurId);
+    foreach ($parties as $partie) {
+        $tempsTotal += $partie['temps_de_jeu'];
+    }
+    return $tempsTotal;
+}
+function jeuLePlusJoue($joueurId) {
+    $jeux = [];
+    $parties = $model->getPartiesByJoueur($joueurId);
+    foreach ($parties as $partie) {
+        $jeuId = $partie['jeu_id'];
+        if (!isset($jeux[$jeuId])) {
+            $jeux[$jeuId] = 0;
+        }
+        $jeux[$jeuId] += $partie['temps_de_jeu'];
+    }
+    arsort($jeux);
+    return key($jeux);
+}
+$tempsDeJeuTotal = cumulerTempsDeJeu($joueurId);
 $classement = $model->getClassement();
 include 'Views/Classement.php';
 ?>
