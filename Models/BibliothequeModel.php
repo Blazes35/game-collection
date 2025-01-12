1dd910d040965cd66595529920249b292412cccb
AyScorp <aymericcherbonnier@gmail.com>
2025-01-12 18:59:12 +0100

    Ajout de la fonctionnalité de bibliothèque pour afficher les jeux d'un joueur et ses données personnelles

---
+++ Models/BibliothequeModel.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'DBModel.php';
+
+class HomeModel extends DBModel {
+    public function getPlayerData($playerId) {
+        $stmt = $this->getDB()->prepare("SELECT prenom, nom FROM utilisateur WHERE id = ?");
+        $stmt->execute([$playerId]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function getPlayerGames($playerId) {
+        $stmt = self::$db->prepare("
+            SELECT jeu.id, jeu.nom, jeu.image_url, jeu.plateforme, jeu.description, jeu.editeur, jeu.date_sortie, utilisateur_jeu.temps_de_jeu 
+            FROM jeu 
+            JOIN utilisateur_jeu ON jeu.id = utilisateur_jeu.jeu_id
+            WHERE utilisateur_jeu.utilisateur_id = :playerId");
+        $stmt->bindParam(':playerId', $playerId);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+}
+?>
