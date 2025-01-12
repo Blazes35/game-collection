d54c51c09a2bb23d49c92fc83ec50aaf71cff65b
Blazes35 <loridam2@gmail.com>
2025-01-12 16:59:11 +0100

    addGameDetail.php

--- Controllers/GameDetailController.php
+++ Controllers/GameDetailController.php
@@ -6,6 +6,15 @@ $gameId = $_SESSION['gameId'];
 $game = $model->getGame($gameId);
 var_dump($game);
 $gameName = $game['nom'];
+$gameImage = $game['image_url'];
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    if(isset($_POST['temps_de_jeu'])){
+        $model->changeTime($_SESSION['id'], $gameId, $_POST['temps_de_jeu']);
+        header('Location: /Profil');
+        exit();
+    }
+}
 
 require 'Views/GameDetailView.php';
 ?>
--- Views/GameDetailView.php
+++ Views/GameDetailView.php
@@ -8,8 +8,9 @@ ob_start();
 <h1>Changer durée de jeu</h1>
 <form action='/GameDetail' method='post' class="add-game-form">
     <p><?php echo $gameName ?></p>
+    <img src="<?php echo $gameImage ?>" alt="Image du jeu">
     <input type="number" name="temps_de_jeu" class='input-field' placeholder="Temps de jeu">
-    <button type="submit" name="temps_de_jeu" class='btn' >MODIFIER</button>
+    <button type="submit" class='btn' >Valider</button>
 </form>
 <?php
 $content = ob_get_clean();
